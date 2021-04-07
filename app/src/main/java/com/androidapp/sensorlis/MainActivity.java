82f1f05fc49184bb66890f51ccc5cf8ede7177d3
sardor <mirzahmedovs266@gmail.com>
2021-04-07 13:30:15 +0900

    homework week 6 day 1 file 1 #1 completed (getsensorlist branch marge 시키고 삭제 완료)

---
+++ app/src/main/java/com/androidapp/sensorlis/MainActivity.java
@@ -0,0 +1,34 @@
+package com.androidapp.sensorlis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mTxtSensor;
+    SensorManager sensorManager;
+    List<Sensor> list;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTxtSensor = findViewById(R.id.mtxtSensor);
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        list = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        mTxtSensor.append("#Sensors:" + list.size()+") \n\n");
+        for (Sensor sensor:list)
+        {
+            mTxtSensor.append("Sensor name" + sensor.getName() + "\n");
+            mTxtSensor.append("Sensor type" + sensor.getType() + "\n\n");
+        }
+    }
+}
