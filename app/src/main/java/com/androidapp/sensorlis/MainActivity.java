722b3c92d017ba5c2d4596adae6411929c77c6ef
sardor <mirzahmedovs266@gmail.com>
2021-04-07 14:03:15 +0900

    homework week 6 day 1 file completed (scrollableSensor list branch marge 시키고 삭제 완료)

--- app/src/main/java/com/androidapp/sensorlis/MainActivity.java
+++ app/src/main/java/com/androidapp/sensorlis/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTxtSensor = findViewById(R.id.mtxtSensor);
+        mTxtSensor.setMovementMethod(new ScrollingMovementMethod());
         sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         list = sensorManager.getSensorList(Sensor.TYPE_ALL);
 
